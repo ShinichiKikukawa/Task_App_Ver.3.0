2743f1fcb0e9cefde1fd50c35d1ce39427fa7738
EC2 Default User <ec2-user@ip-172-31-8-151.ap-northeast-1.compute.internal>
2022-08-28 17:57:03 +0000

    B8日本語化を残すのみ

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,6 +10,8 @@ module AttendanceApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
