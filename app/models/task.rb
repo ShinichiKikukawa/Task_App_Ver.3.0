ef7932157946fd30e87342ca7fc52d5a04377588
EC2 Default User <ec2-user@ip-172-31-8-151.ap-northeast-1.compute.internal>
2022-09-10 20:09:10 +0000

    Finish C13

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
  
   belongs_to :user
+  
+  validates :user_id, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { in: 5..300 }
 end
