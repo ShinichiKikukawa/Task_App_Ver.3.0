8bf5b0b144b64a1856ae1289f6e2088df81543cb
EC2 Default User <ec2-user@ip-172-31-8-151.ap-northeast-1.compute.internal>
2022-09-18 12:33:12 +0000

    Finish Task_App

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:show, :edit, :update,:destroy]
-before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
-before_action :admin_user, only: [:index, :destroy]
-before_action :correct_user, only: [:edit, :update]
-before_action :admin_or_correct, only: [:show]
+  before_action :set_user, only: %i(show edit update destroy)
+  before_action :logged_in_user, only: %i(index show edit update destroy)
+  before_action :admin_user, only: %i(index destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_or_correct, only: %i(show)
 
   def index
     @users = User.paginate(page: params[:page], per_page: 20)
